5592531dd2d7964bf5c0f17c8e2be5ee4b5de18c
WesamAlqudah <100211002+WesamAlqudah@users.noreply.github.com>
2022-04-14 02:50:46 -0500

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
+import Post from './components/Post/Post';
+import Dashboard from './containers/dashboard/Dashboard';
+import Posts from '../../containers/Posts/Posts';
 
 function App() {
   return (
     <div className="App">
-     <h3>MIU </h3>
-     
+      <h3>MIU </h3>
+      <Dashboard />
     </div>
   );
 }
