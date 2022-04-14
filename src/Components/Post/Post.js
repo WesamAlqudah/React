abeff4641527ee2571c1fa174f4aea95e78729db
WesamAlqudah <100211002+WesamAlqudah@users.noreply.github.com>
2022-04-14 03:57:18 -0500

    .

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -3,10 +3,14 @@ const Post = (props) => {
 
     return (
         <div className="Content">
+            {/* <div className="Field">
+                {props.id}
+            </div> */}
             <h1> {props.title}</h1>
             <div className="Field">
                 {props.author}
             </div>
+          
         </div>
     );
 }
